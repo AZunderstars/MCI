a09e134e78207c9f2484818372685685048b1b9a
AZunderstars <fidchetspinner@gmail.com>
2024-09-27 13:07:26 +0330

    added argument check to RangeIPValue constructor

--- src/RangeIPValue.cpp
+++ src/RangeIPValue.cpp
@@ -1,8 +1,14 @@
 #include "RangeIPValue.hpp"
+#include "utils.hpp"
+#include <stdexcept>
 
 using namespace std;
 
 RangeIPValue::RangeIPValue(string _value)
 {
+    if (!is_range_IP_value_valid(_value))
+    {
+        throw invalid_argument("invalid range IP value.");
+    }
     value = _value;
 }
