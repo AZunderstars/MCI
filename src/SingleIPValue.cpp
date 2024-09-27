640ffed5dc4cc2b0aaaff9c844e935b70a5f01ec
AZunderstars <fidchetspinner@gmail.com>
2024-09-27 15:08:56 +0330

    removed exception handling in IPValue constructors

--- src/SingleIPValue.cpp
+++ src/SingleIPValue.cpp
@@ -1,14 +1,8 @@
 #include "SingleIPValue.hpp"
-#include "utils.hpp"
-#include <stdexcept>
 
 using namespace std;
 
 SingleIPValue::SingleIPValue(string _value)
 {
-    if (!is_single_IP_value_valid(value))
-    {
-        throw invalid_argument("invalid single IP value.");
-    }
     value = _value;
 }
--- src/SubnetIPValue.cpp
+++ src/SubnetIPValue.cpp
@@ -1,14 +1,8 @@
 #include "SubnetIPValue.hpp"
-#include "utils.hpp"
-#include <stdexcept>
 
 using namespace std;
 
 SubnetIPValue::SubnetIPValue(string _value)
 {
-    if (!is_subnet_IP_value_valid(_value))
-    {
-        throw invalid_argument("invalid subnet IP value.");
-    }
     value = _value;
 }
