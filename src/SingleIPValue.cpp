a9fd8c3995ca80b30825a88610e606bee79126f5
AZunderstars <fidchetspinner@gmail.com>
2024-09-27 13:08:40 +0330

    added argument check to SingleIPValue constructor

--- src/SingleIPValue.cpp
+++ src/SingleIPValue.cpp
@@ -1,8 +1,14 @@
 #include "SingleIPValue.hpp"
+#include "utils.hpp"
+#include <stdexcept>
 
 using namespace std;
 
 SingleIPValue::SingleIPValue(string _value)
 {
+    if (!is_single_IP_value_valid(value))
+    {
+        throw invalid_argument("invalid single IP value.");
+    }
     value = _value;
 }
